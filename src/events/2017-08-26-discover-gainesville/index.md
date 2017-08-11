a0b286bd9573ae46324fa52fe4a5bcc9480ada7c
Eran Schoellhorn <eschoellhorn@352inc.com>
2017-08-11 14:47:21 -0400

    Update content and design for more information, add registration link

--- src/events/2017-08-26-discover-gainesville/index.md
+++ src/events/2017-08-26-discover-gainesville/index.md
@@ -3,16 +3,13 @@ path: "/fl/gnv/discover-gainesville"
 title: "Discover Gainesville"
 location: "Gainesville, FL"
 venue: "Infinity Hall"
-address: "978 SW 2nd Ave., Gainesville, FL 32601"
+address: "978 SW 2nd Ave.\nGainesville, FL 32601"
 date: "2017-08-26"
 time: "9:00am - 6:00pm EST"
 registrationUrl: "https://hackbattles.typeform.com/to/QwEfjF"
 ---
 
 ## Information
-Hosted at: [Infinity Hall](https://www.google.com/maps/place/University+of+Florida:+Infinity+Hall/@29.6504469,-82.3349001,15z/data=!4m5!3m4!1s0x0:0x779e93744482a185!8m2!3d29.6504469!4d-82.3349001?sa=X&ved=0ahUKEwiY6eGLv8XVAhVC72MKHRS6AZ4Q_BIIgQEwCg)
-Saturday, August 26th, 2017
-
 Join #hackgnv on the [GNV Connect](https://gnvconnect.com/) Slack Group for discussions.
 
 ## Introduction
--- src/layouts/event.js
+++ src/layouts/event.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet';
 import graphql from 'graphql-tag';
 
 import Header from '../components/Header';
+import GoogleStaticMap from '../components/GoogleStaticMap';
 
 const EventTemplate = ({ data }) => {
   const { markdownRemark: post } = data;
@@ -15,11 +16,32 @@ const EventTemplate = ({ data }) => {
           byline={post.frontmatter.location}
         />
         <div className="event__body">
+          <div className="event__meta">
+            <dl className="event__details">
+              <dt>Date:</dt>
+              <dd>{post.frontmatter.date}</dd>
+              <dt>Time:</dt>
+              <dd>{post.frontmatter.time}</dd>
+              <dt>Venue:</dt>
+              <dd>{post.frontmatter.venue}</dd>
+              <dt>Address:</dt>
+              <dd>{post.frontmatter.address}</dd>
+            </dl>
+            <GoogleStaticMap
+              address={post.frontmatter.address}
+            />
+          </div>
           <div
-            className="blog-post-content"
+            className="event__content"
             dangerouslySetInnerHTML={{ __html: post.html }}
           />
         </div>
+        { post.frontmatter.registrationUrl && (
+          <a
+            className="button button--block"
+            href={post.frontmatter.registrationUrl}
+          >Register Your Team Now!</a>
+        )}
       </div>
     </div>
   );
@@ -35,6 +57,11 @@ export const pageQuery = graphql`
                 path
                 title
                 location
+                address
+                date
+                time
+                registrationUrl
+                venue
             }
         }
     }
